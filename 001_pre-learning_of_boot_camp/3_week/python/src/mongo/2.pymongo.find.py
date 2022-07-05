79d68ea2bad4cd733d1e1992c5cd33bf84e49c00
Unchaptered <workstation19961002@gmail.com>
2022-07-05 19:53:25 +0900

    Fix syntax warning

--- 001_pre-learning_of_boot_camp/3_week/python/src/mongo/2.pymongo.find.py
+++ 001_pre-learning_of_boot_camp/3_week/python/src/mongo/2.pymongo.find.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 client = MongoClient('Mongo URL')
 db = client.dbsparta 
 
-targetMovie = db.movies.find_one({ 'title': '가버나움' });
+targetMovie = db.movies.find_one({ 'title': '가버나움' })
 resultMovie = db.movies.find_one({ 'point': targetMovie['point'] })
 
 print(targetMovie)
