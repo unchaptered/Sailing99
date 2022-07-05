97c460c506e44bf100b9830af993d1932d653ce3
Unchaptered <workstation19961002@gmail.com>
2022-07-05 19:49:03 +0900

    Add 3 week, find with pymongo

---
+++ 001_pre-learning_of_boot_camp/3_week/python/src/mongo/2.crolling.usage.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+client = MongoClient('Mongo URL')
+db = client.dbsparta 
+
+targetMovie = db.movies.find_one({ 'title': '가버나움' });
+resultMovie = db.movies.find_one({ 'point': targetMovie['point'] })
+
+print(targetMovie)
+print(resultMovie)
