62d01d5091889abdc007260063f4ce1f33cf3e45
Unchaptered <workstation19961002@gmail.com>
2022-07-05 22:26:47 +0900

    Add server, `mars`, using flask

--- 001_pre-learning_of_boot_camp/4_week/mars/app.py
+++ 001_pre-learning_of_boot_camp/4_week/mars/app.py
@@ -1,19 +1,32 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = MongoClient('Mongo URL')
+db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/mars", methods=["POST"])
+@app.route('/mars', methods=['POST'])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    db.mars.insert_one({
+        'name': request.form['name'],
+        'address': request.form['address'],
+        'size': request.form['size']
+    })
+
+    return jsonify({'msg': '주문 완료!'})
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    return jsonify({
+        'orders': list(
+            db.mars.find({}, {"_id": False})
+        )
+    })
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
